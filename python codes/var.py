79062b6dd55f84a41b4ef79de8cf546fa86221b5
MadhumithaMCA <144420413+MadhumithaMCA@users.noreply.github.com>
2025-02-02 23:46:38 +0530

    Add files via upload

---
+++ python codes/var.py
@@ -0,0 +1,40 @@
+"""
+x =5
+y = "hello world"
+
+print(x,y)
+
+"""
+""""
+x = 5
+y = 3
+z = x+y
+
+print(z)
+
+"""
+
+
+"""
+creating variables
+
+
+x =5
+y = "john"
+
+print(x)
+print(y)
+
+"""
+"""
+casting
+
+"""
+
+x = int(3)
+y = str(3)
+z = float(3)
+
+print(x)
+print(y)
+print(z)
