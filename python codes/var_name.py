4dc8ab62e52aa571ffabe048f2c5889d70715a68
MadhumithaMCA <144420413+MadhumithaMCA@users.noreply.github.com>
2025-02-04 23:15:34 +0530

    Add files via upload

---
+++ python codes/var_name.py
@@ -0,0 +1,19 @@
+#camel case  Each word, except the first, starts with a capital letter
+
+myVariableName = "john"
+
+print(myVariableName)
+
+# pascal case  Each word starts with a capital letter:
+
+MyVariableName = "Anna"
+
+print(MyVariableName)
+
+
+#Snake Case  Each word is separated by an underscore character
+
+my_variable_name = "helen"
+
+print(my_variable_name)
+
